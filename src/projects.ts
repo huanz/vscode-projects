1e4a178bae28d92f0cbbf6254e7556e703e25fd6
bukas <yhz1219@gmail.com>
2016-12-21 19:43:47 +0800

    first commit

--- src/projects.ts
+++ src/projects.ts
@@ -21,6 +21,7 @@ export default class Projects {
     public homeDir: string = os.homedir()
 
     private context: vscode.ExtensionContext
+    private config: vscode.WorkspaceConfiguration = workspace.getConfiguration('projects')
 
     public constructor(context: vscode.ExtensionContext) {
         this.context = context;
@@ -28,7 +29,7 @@ export default class Projects {
         this.showStatusBar();
     }
     public showStatusBar(): void {
-        let showStatusBar = workspace.getConfiguration('projectManager').get('showProjectNameInStatusBar');
+        let showStatusBar = this.config.get('showProjectNameInStatusBar');
         let currentProjectPath = workspace.rootPath;
         if (showStatusBar && currentProjectPath) {
             let statusItem: vscode.StatusBarItem = window.createStatusBarItem(StatusBarAlignment.Left);
@@ -36,24 +37,61 @@ export default class Projects {
             statusItem.tooltip = currentProjectPath;
             statusItem.command = 'projects.list';
 
-            let projects: ProjectElement[] = this.loadProjects();
+            let projects: ProjectElement[] = this.getProjects();
             currentProjectPath = currentProjectPath.toString().toLowerCase();
-            let currentProject:ProjectElement = projects.find((project) => project.path.toString().toLowerCase() === currentProjectPath);
+            let currentProject: ProjectElement = projects.find((project) => project.path.toString().toLowerCase() === currentProjectPath);
             if (currentProject) {
-	            statusItem.text += currentProject.name;
-	            statusItem.show();
-	        }
+                statusItem.text += currentProject.name;
+                statusItem.show();
+            }
         }
     }
     public registerCommands(): void {
-        this.context.subscriptions.push(commands.registerCommand('projects.list', () => this.loadProjects));
-        
+        this.context.subscriptions.push(commands.registerCommand('projects.list', () => this.listProjects));
     }
-    public loadProjects(): ProjectElement[] {
-        return [];
+    public listProjects() {
+        let projects: ProjectElement[] = this.getProjects();
+        let options = <vscode.QuickPickOptions>{
+            placeHolder: 'Loading Projects (pick one to open)',
+            matchOnDescription: false,
+            matchOnDetail: false
+        };
+
+        window.showQuickPick(projects, options).then(
+            selected => this.pickProject(selected),
+            () => this.showInfo('Error loading projects: ${reason}')
+        );
+    }
+    private pickProject(selected?: ProjectElement) {
+        if (!selected) {
+            return;
+        }
+        let openInNewWindow: boolean = this.config.get('openInNewWindow', true);
+        let uri: vscode.Uri = vscode.Uri.file(selected.path);
+        commands.executeCommand('vscode.openFolder', uri, openInNewWindow)
+            .then(
+                value => ({}),
+                value => this.showInfo('Could not open the project!')
+            );
+    }
+    public getProjects(): ProjectElement[] {
+        let projectDir = this.getProjectPath();
+        if (projectDir) {
+            let ignoredFolders = this.config.get('ignoredFolders', []);
+            return fs.readdirSync(projectDir).filter(function (dir) {
+                return !dir.startsWith('.') && ignoredFolders.indexOf(dir) === -1 && fs.statSync(path.join(projectDir, dir)).isDirectory();
+            }).map(function (dir) {
+                return {
+                    name: dir,
+                    path: path.join(projectDir, dir)
+                };
+            });
+        } else {
+            this.showError('Error loading project dir');
+        }
     }
     public getProjectPath(): any {
-        let projectsLocation: string = workspace.getConfiguration('projectManager').get<string>('projectsLocation');
+        let projectsLocation: string = this.config.get<string>('projectsLocation');
         projectsLocation = projectsLocation ? this.replaceHome(projectsLocation) : path.join(this.homeDir, 'projects');
         try {
             let stats = fs.statSync(projectsLocation);
@@ -74,5 +112,8 @@ export default class Projects {
     }
     public showError(msg: string, option?: vscode.MessageItem): Thenable<any> {
         return window.showErrorMessage(msg, option);
+    }
+    public showInfo(msg: string): void {
+        window.showInformationMessage(msg);
     }
 }
